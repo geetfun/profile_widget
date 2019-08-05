5f8f54236eba0344ea5d74496d7b310c8923de7f
Simon Chiu <simononstartups@gmail.com>
2019-08-05 13:35:26 -0400

    Can add questions

--- app/controllers/profile_widget/admin/surveys_controller.rb
+++ app/controllers/profile_widget/admin/surveys_controller.rb
@@ -10,6 +10,20 @@ module ProfileWidget
       end
     end
 
+    def active
+      @surveys = ProfileWidget::Survey.where(active: true).page(params[:page]).per(10)
+      respond_to do |format|
+        format.html { render "index" }
+      end
+    end
+
+    def inactive
+      @surveys = ProfileWidget::Survey.where(active: false).page(params[:page]).per(10)
+      respond_to do |format|
+        format.html { render "index" }
+      end
+    end
+
     def new
       @survey = ProfileWidget::Survey.new
       respond_to do |format|
@@ -18,29 +32,51 @@ module ProfileWidget
     end
 
     def create
-
+      @survey = ProfileWidget::Survey.new(survey_params)
+      respond_to do |format|
+        if @survey.save
+          format.html { redirect_to admin_survey_path(@survey), notice: "Survey created" }
+        else
+          flash.now[:alert] = "Please try again"
+          format.html { render "new" }
+        end
+      end
     end
 
     def show
-
+      respond_to do |format|
+        format.html
+      end
     end
 
     def edit
-
+      respond_to do |format|
+        format.html
+      end
     end
 
     def update
-
+      respond_to do |format|
+        if @survey.update(survey_params)
+          format.html { redirect_to admin_survey_path(@survey), notice: "Survey created" }
+        else
+          flash.now[:alert] = "Please try again"
+          format.html { render "new" }
+        end
+      end
     end
 
     def destroy
-
+      @survey.destroy
+      respond_to do |format|
+        format.html { redirect_to admin_surveys_path, notice: "Survey destroyed" }
+      end
     end
 
     private
 
     def set_survey
-
+      @survey = ProfileWidget::Survey.find(params[:id])
     end
 
     def survey_params
